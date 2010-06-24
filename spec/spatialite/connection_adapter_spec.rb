0c8af95c80298bc32ebf7dd6954ba4220e372a4b
Pirmin Kalberer <pka@sourcepole.ch>
2010-06-25 00:47:22 +0200

    Some tests implemented

--- spec/spatialite/connection_adapter_spec.rb
+++ spec/spatialite/connection_adapter_spec.rb
@@ -10,71 +10,54 @@ describe "Modified SQlite3Adapter" do
     @connection = ActiveRecord::Base.connection
   end
   
-  describe '#postgis_version' do
+  describe '#opengis_version' do
     it 'should report a version number if PostGIS is installed' do
-      @connection.should_receive(:select_value).with('SELECT postgis_full_version()').and_return('POSTGIS="1.5.0" GEOS="3.2.0-CAPI-1.6.0" PROJ="Rel. 4.7.1, 23 September 2009" LIBXML="2.7.6" USE_STATS')
-      @connection.postgis_version.should_not be_nil
+      @connection.should_receive(:select_value).with('SELECT spatialite_version()').and_return('2.4.0')
+      @connection.opengis_version.should_not be_nil
     end
     
     it 'should report nil if PostGIS is not installed' do
-      @connection.should_receive(:select_value).with('SELECT postgis_full_version()').and_raise(ActiveRecord::StatementInvalid)
-      @connection.postgis_version.should be_nil
+      @connection.should_receive(:select_value).with('SELECT spatialite_version()').and_raise(ActiveRecord::StatementInvalid)
+      @connection.opengis_version.should be_nil
     end
   end
   
-  describe '#postgis_major_version' do
+  describe '#opengis_major_version' do
     it 'should be the first component of the version number' do
-      @connection.stub!(:postgis_version).and_return('1.5.0')
-      @connection.postgis_major_version.should == 1
+      @connection.stub!(:opengis_version).and_return('1.5.0')
+      @connection.opengis_major_version.should == 1
     end
     
     it 'should be nil if PostGIS is not installed' do
-      @connection.stub!(:postgis_version).and_return(nil)
-      @connection.postgis_major_version.should be_nil
+      @connection.stub!(:opengis_version).and_return(nil)
+      @connection.opengis_major_version.should be_nil
     end
   end
   
-  describe '#postgis_minor_version' do
+  describe '#opengis_major_version' do
     it 'should be the second component of the version number' do
-      @connection.stub!(:postgis_version).and_return('1.5.0')
-      @connection.postgis_minor_version.should == 5
+      @connection.stub!(:opengis_version).and_return('1.5.0')
+      @connection.opengis_minor_version.should == 5
     end
     
     it 'should be nil if PostGIS is not installed' do
-      @connection.stub!(:postgis_version).and_return(nil)
-      @connection.postgis_minor_version.should be_nil
+      @connection.stub!(:opengis_version).and_return(nil)
+      @connection.opengis_minor_version.should be_nil
     end
   end
   
   describe '#spatial?' do
     it 'should be true if PostGIS is installed' do
-      @connection.should_receive(:select_value).with('SELECT postgis_full_version()').and_return('POSTGIS="1.5.0" GEOS="3.2.0-CAPI-1.6.0" PROJ="Rel. 4.7.1, 23 September 2009" LIBXML="2.7.6" USE_STATS')
+      @connection.should_receive(:select_value).with('SELECT spatialite_version()').and_return('2.4.0')
       @connection.should be_spatial
     end
     
     it 'should be false if PostGIS is not installed' do
-      @connection.should_receive(:select_value).with('SELECT postgis_full_version()').and_raise(ActiveRecord::StatementInvalid)
+      @connection.should_receive(:select_value).with('SELECT spatialite_version()').and_raise(ActiveRecord::StatementInvalid)
       @connection.should_not be_spatial
     end
   end
   
-  describe '#supports_geographic?' do
-    it "should be true for PostGIS version 1.5.0" do
-      @connection.stub!(:postgis_version).and_return('1.5.0')
-      @connection.supports_geographic?.should == true
-    end
-    
-    it "should be true for PostGIS newer than 1.5.0" do
-      @connection.stub!(:postgis_version).and_return('1.5.1')
-      @connection.supports_geographic?.should == true
-    end
-    
-    it "should be true for PostGIS older than 1.5.0" do
-      @connection.stub!(:postgis_version).and_return('1.4.0')
-      @connection.supports_geographic?.should == false
-    end
-  end
-
   describe "#columns" do
     describe "type" do
       it "should be a regular SpatialPostgreSQLColumn if column is a geometry data type" do
@@ -129,37 +112,6 @@ describe "Modified SQlite3Adapter" do
         GeometryModel.columns.select{|c| c.name == 'geom'}.first.geometry_type.should == :geometry
       end
       
-      it "should be :point for geography columns restricted to POINT types" do
-        GeographyPointModel.columns.select{|c| c.name == 'geom'}.first.geometry_type.should == :point
-      end
-      
-      it "should be :line_string for geography columns restricted to LINESTRING types" do
-        GeographyLineStringModel.columns.select{|c| c.name == 'geom'}.first.geometry_type.should == :line_string
-      end
-
-      it "should be :polygon for geography columns restricted to POLYGON types" do
-        GeographyPolygonModel.columns.select{|c| c.name == 'geom'}.first.geometry_type.should == :polygon
-      end
-
-      it "should be :multi_point for geography columns restricted to MULTIPOINT types" do
-        GeographyMultiPointModel.columns.select{|c| c.name == 'geom'}.first.geometry_type.should == :multi_point
-      end
-
-      it "should be :multi_line_string for geography columns restricted to MULTILINESTRING types" do
-        GeographyMultiLineStringModel.columns.select{|c| c.name == 'geom'}.first.geometry_type.should == :multi_line_string
-      end
-      
-      it "should be :multi_polygon for geography columns restricted to MULTIPOLYGON types" do
-        GeographyMultiPolygonModel.columns.select{|c| c.name == 'geom'}.first.geometry_type.should == :multi_polygon
-      end
-      
-      it "should be :geometry_collection for geography columns restricted to GEOMETRYCOLLECTION types" do
-        GeographyGeometryCollectionModel.columns.select{|c| c.name == 'geom'}.first.geometry_type.should == :geometry_collection
-      end
-      
-      it "should be :geometry for geography columns not restricted to a type" do
-        GeographyModel.columns.select{|c| c.name == 'geom'}.first.geometry_type.should == :geometry
-      end
     end
   end
   
@@ -182,11 +134,6 @@ describe "Modified SQlite3Adapter" do
     
     it "should be marked as spatial if a GiST index on a geometry column" do
       @indexes.select{|i| i.name == 'index_point_models_on_geom'}.first.spatial.should == true
-    end
-    
-    it "should be marked as spatial if a GiST index on a geography column" do
-      @indexes = @connection.indexes('geography_point_models')
-      @indexes.select{|i| i.name == 'index_geography_point_models_on_geom'}.first.spatial.should == true
     end
     
     it "should not be marked as spatial if not a GiST index" do
