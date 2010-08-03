7631d2b905394d8853135ee4a91cd9ae7cb26178
Kashif Rasul <kashif@nomad-labs.com>
2010-08-03 12:42:48 +0200

    Removed messy spec_helper requires

--- spec/spatialite/models_spec.rb
+++ spec/spatialite/models_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'spec_helper'
 require 'spatial_adapter/sqlite3'
 require 'db/spatialite_raw'
 require 'models/common'
--- spec/spatialite/schema_dumper_spec.rb
+++ spec/spatialite/schema_dumper_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'spec_helper'
 require 'spatial_adapter/sqlite3'
 
 describe "Spatially-enabled Schema Dumps" do
